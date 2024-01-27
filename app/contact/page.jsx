873f80cd9b75cdf0163ff197e7d42b7e7d8c4cc0
Abdelhak MEKAOUI <mekaouiabdelhak1@gmail.com>
2024-01-27 12:42:41 +0100

    adding contact pages

---
+++ app/contact/page.jsx
@@ -0,0 +1,169 @@
+'use client'
+import { useState } from 'react'
+import Datepicker from "tailwind-datepicker-react"
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const options = {
+	title: "Incident date",
+	autoHide: true,
+	todayBtn: true,
+	clearBtn: true,
+	clearBtnText: "Clear",
+	maxDate: new Date("2030-01-01"),
+	minDate: new Date("1950-01-01"),
+	theme: {
+		background: "bg-[#fff]",
+		todayBtn: "",
+		clearBtn: "",
+		icons: "",
+		text: "text-1 font-light",
+		disabledText: "",
+		input: "",
+		inputIcon: "",
+		selected: "",
+	},
+	icons: {
+		prev: () => <span>{'<'}</span>,
+		next: () => <span>{'>'}</span>,
+	},
+	datepickerClassNames: "h-36",
+	defaultDate: new Date("2022-01-01"),
+	language: "en",
+	disabledDates: [],
+	weekDays: ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+	inputNameProp: "date",
+	inputIdProp: "date",
+	inputPlaceholderProp: "Select Date",
+	inputDateFormatProp: {
+		day: "numeric",
+		month: "long",
+		year: "numeric"
+	}
+}
+
+export default function Example() {
+  // const router = useRouter();
+  const [show, setShow] = useState(false)
+	const handleChange = (selectedDate) => {
+		console.log(selectedDate)
+	}
+	const handleClose = (state) => {
+		setShow(state)
+	}
+
+  // const LoadNext = () => {
+  //   router.push('/contact2')
+  // }
+
+  return (
+    <div className="flex flex-raw w-full items-center justify-around bg-white lg:px-36 xl:gap-4 xl:px-40">
+        <div className="flex items-center justify-center">
+            <img
+             className='hidden lg:block'
+             src="./form_img.png" alt="" />
+        </div>
+
+        <div className="isolate bg-white px-2 w-full py-12 sm:py-16   lg:max-h-screen">
+      <div className="mx-auto max-w-2xl text-center flex flex-raw gap-6 justify-center lg:gap-12">
+        <div className="border solid rounded-full bg-green-400 px-4 py-4 flex items-center justify-center w-12 h-12 font-bold text-8  text-white">1</div>
+        <div className="border-2 solid border-blue-600 rounded-full bg-white px-4 py-4 flex items-center justify-center w-12 h-12 font-bold text-8 text-blue-600">2</div>
+        <div className="border-2 solid border-blue-600 rounded-full bg-white px-4 py-4 flex items-center justify-center w-12 h-12 font-bold text-8 text-blue-600">3</div>
+      </div>
+
+      <form action="#" method="POST" className="mx-auto  mt-16 max-w-xl sm:mt-20">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label className="w-full max-w-xs">
+                  <div className="label">
+                      <span className="label-text">Where you injured ?</span>
+                  </div>
+                  <select className=" block w-full select select-bordered">
+                      <option disabled selected>Pick one</option>
+                      <option>Yes</option>
+                      <option>No</option>
+                
+                  </select>
+              </label>
+          </div>
+
+          <div >
+            <label className="w-full  max-w-xs">
+                  <div className="label">
+                      <span className="label-text">Where you injured ?</span>
+                  </div>
+                  <Datepicker classNames='block w-full' options={options} onChange={handleChange} show={show} setShow={handleClose} />
+              </label>
+          </div>
+          
+            <div>
+            <label className="w-full max-w-xs">
+                  <div className="label">
+                      <span className="label-text">Where you injured ?</span>
+                  </div>
+                  <select className=" block w-full select select-bordered">
+                      <option disabled selected>Pick one</option>
+                      <option>Yes</option>
+                      <option>No</option>
+                
+                  </select>
+              </label>
+          </div>
+          <div>
+            <label className="w-full max-w-xs">
+                  <div className="label">
+                      <span className="label-text">Where you injured ?</span>
+                  </div>
+                  <select className=" block w-full select select-bordered">
+                      <option disabled selected>Pick one</option>
+                      <option>Yes</option>
+                      <option>No</option>
+                
+                  </select>
+              </label>
+          </div>
+          <div>
+            <label className="w-full max-w-xs">
+                  <div className="label">
+                      <span className="label-text">Where you injured ?</span>
+                  </div>
+                  <select className=" block w-full select select-bordered">
+                      <option disabled selected>Pick one</option>
+                      <option>Yes</option>
+                      <option>No</option>
+                
+                  </select>
+              </label>
+          </div>
+            <div className="sm:col-span-2">
+            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+              Message
+            </label>
+            <div className="mt-2.5">
+              <textarea
+                name="message"
+                id="message"
+                rows={4}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={''}
+              />
+            </div>
+          </div>
+        
+
+          
+      
+          
+        </div>
+        <div className="mt-10 flex justify-center">
+          <Link href="/contact2"
+            className="block w-[30%] rounded-2xl bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+          Next
+          </Link>
+        </div>
+      </form>
+    </div>
+    </div>
+  )
+}
---
+++ pages/contact4/page.jsx
@@ -0,0 +1,197 @@
+'use client'
+import { useState } from 'react'
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  const [agreed, setAgreed] = useState(false)
+
+  return (
+    <div className="imin-h-screen flex flex-raw w-full items-center justify-around lg:justify-center bg-white lg:px-8">
+        <div>
+            <img
+             className='hidden lg:block'
+             src="./form_img.png" alt="" />
+        </div>
+        <div className="isolate bg-white px-2 w-full py-24 sm:py-32 lg:px-4 lg:max-h-screen">
+
+      <div className="mx-auto max-w-2xl text-center flex flex-raw gap-6 justify-center lg:gap-12">
+        <div className="border solid rounded-full bg-green-400 px-4 py-4 flex items-center justify-center w-12 h-12 font-bold text-8  text-white">1</div>
+        <div className="border solid rounded-full bg-green-400 px-4 py-4 flex items-center justify-center w-12 h-12 font-bold text-8  text-white">2</div>
+        <div className="border-2 solid border-blue-600 rounded-full bg-white px-4 py-4 flex items-center justify-center w-12 h-12 font-bold text-8 text-blue-600">3</div>
+      </div>
+      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+              First name
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+              Last name
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+              Email
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                autoComplete="email"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div >
+            <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+              Phone number 1
+            </label>
+            <div className="relative mt-2.5">
+              <div className="absolute inset-y-0 left-0 flex items-center">
+                <label htmlFor="country" className="sr-only">
+                  Country
+                </label>
+                <select
+                  id="country"
+                  name="country"
+                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                >
+                  <option>US</option>
+                  <option>CA</option>
+                  <option>EU</option>
+                </select>
+           
+              </div>
+              <input
+                type="tel"
+                name="phone-number"
+                id="phone-number"
+                autoComplete="tel"
+                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+              Phone number 2
+            </label>
+            <div className="relative mt-2.5">
+              <div className="absolute inset-y-0 left-0 flex items-center">
+                <label htmlFor="country" className="sr-only">
+                  Country
+                </label>
+                <select
+                  id="country"
+                  name="country"
+                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                >
+                  <option>US</option>
+                  <option>CA</option>
+                  <option>EU</option>
+                </select>
+           
+              </div>
+              <input
+                type="tel"
+                name="phone-number"
+                id="phone-number"
+                autoComplete="tel"
+                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+              Address
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className='sm:col-span-2'>
+            <label className="w-full max-w-xs">
+                  <div className="label">
+                      <span className="label-text">City</span>
+                  </div>
+                  <select className=" block w-full select select-bordered">
+                      <option disabled selected>Pick one</option>
+                      <option>Yes</option>
+                      <option>No</option>
+                
+                  </select>
+              </label>
+          </div>
+
+          <div>
+            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+              State
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+              Zipcode
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+        
+          
+        </div>
+        <div className="mt-10 flex justify-center">
+          <button
+            type="submit"
+            className="block w-[30%] rounded-2xl bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Next
+          </button>
+        </div>
+      </form>
+      </div>
+    </div>
+  )
+}
